2e98b744359953840c0cec76953476e795acea50
icebergmilk <82156726+icebergmilk@users.noreply.github.com>
2021-06-29 16:57:14 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ function computerPlay() {
     let rand = Math.floor(Math.random() * 3);
     return ITEMS[rand];
 }
+
+function playRound(playerSelection, computerSelection) {
+    const p = playerSelection.toLowerCase();
+    const c = computerSelection.toLowerCase();
+    let result;
+    let winner;
+    let loser;
+
+    if (p === c) {
+        return 'It\'s a tie!';
+    }
+
+    if ((p === 'rock' && c === 'scissors') ||
+        (p === 'scissors' && c === 'paper') ||
+        (p === 'paper' && c === 'rock')) {
+        result = 'You WIN!';
+        winner = p;
+        loser = c;
+    } else {
+        result = 'You LOSE!';
+        winner = c;
+        loser = p;
+    }
+
+    return `${result} ${capitaliseFirstLetter(winner)} beats ${capitaliseFirstLetter(loser)}`;
+}
+
+function capitaliseFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
